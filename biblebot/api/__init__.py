e8a9139336c14fc63119b6df30cc33d7b7951a49
Brave Kim <ygflove@likelion.org>
2021-03-03 21:30:07 +0900

    :wastebasket: remove LoginUseLambda class
    로그인을 통한 세션 관리를 람다에서함

--- biblebot/api/__init__.py
+++ biblebot/api/__init__.py
@@ -19,7 +19,6 @@ from .mileage import Login as MileageLogin
 from .mileage import Search as MileageSearch
 from .mileage import Statement as MileageStatement
 from .mileage import ScrapUseLambda as MileageScrapUseLambda
-from .mileage import LoginUseLambda as MileageLoginUseLambda
 from ._mileage import SearchParamData as MileageSearchParamData
 from ._mileage import StatementParamData as MileageStatementParamData
 from .library import Login as LibraryLogin
@@ -67,7 +66,6 @@ class MileageAPI:
     Search = MileageSearch
     Statement = MileageStatement
     ScrapUseLambda = MileageScrapUseLambda
-    LoginUseLambda = MileageLoginUseLambda
 
 
 class MileageParam:
